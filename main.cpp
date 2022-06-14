4c067804ede31bfe478876e5b0353b4887b468af
nguyenduclok <89176269+nguyenduclok@users.noreply.github.com>
2022-06-14 11:42:41 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-// #define EXERCISE
 #define F				first
 #define S 				second
 #define endl				"\n"
@@ -25,10 +24,6 @@ int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
-#ifndef EXERCISE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-#endif
 	int t = 1;
 	// cin >> t;
 	while(t--) solve();
